8c2d5a70e00b244bee5ae9a561bbcb9da3ecdf2f
shawn10067 <shawn10067@gmail.com>
2022-06-18 02:48:29 -0400

    sheen message added

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
+      <Text>It's Sheen!</Text>
       <StatusBar style="auto" />
     </View>
   );
