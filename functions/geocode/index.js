e809e445a6c13ae44901112e4983a83bf7ee0a8e
shawn10067 <shawn10067@gmail.com>
2022-07-02 15:05:15 -0400

    began mock service

---
+++ functions/geocode/index.js
@@ -0,0 +1,16 @@
+const { locations: locationMock } = require("./geocode.mock");
+const url = require("url");
+
+module.exports.geocodeRequest = (request, response) => {
+  const { city } = url.parse(request.url, true).query;
+  if (city) {
+    const result = locationMock[city.toLowerCase()];
+    if (result) {
+      response.json(locationMock[city.toLowerCase()]);
+    } else {
+      response.json({ error: "no such city found" });
+    }
+  } else {
+    response.json({ error: "didn't send city info" });
+  }
+};
--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,20 +1,20 @@
 import camelize from "camelize";
-import { locations } from "./location.mock";
+import axios from "axios";
 
 export const locationRequest = (locationSearchTerm) => {
-  return new Promise((resolve, reject) => {
-    const location = locations[locationSearchTerm];
-    if (location) {
-      setTimeout(() => {
-        resolve(location);
-      }, 400);
-    } else {
-      reject("No location with that search term");
-    }
-  });
+  return axios
+    .get("http://localhost:5001/mealstogo-be58e/us-central1/geocode", {
+      params: {
+        city: locationSearchTerm,
+      },
+    })
+    .then((result) => console.log("RES", result.data))
+    .then((result) => (result ? result.data : new Error("No data")))
+    .catch((e) => console.log("Network Error ", e.response));
 };
 
 export const locationTransform = (result) => {
+  console.log("FUNC RES", result);
   const formattedResult = camelize(result);
   const { results = [] } = formattedResult;
   const { geometry = {} } = camelize(results[0]);
